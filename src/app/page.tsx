ae8423bf351fe8b857054a83d8df862b2ffbf5ce
TranTheDang <thedangqn@gmail.com>
2023-12-28 00:15:35 +0700

    Header animation added, but not fully completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
-      <div className="container mx-auto px-12 py-4">{/* <Hero></Hero> */}</div>
+      <div className="container mx-auto px-12 py-4 mt-60">
+        <Hero></Hero>
+      </div>
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,76 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { HiMiniBars3BottomRight } from "react-icons/hi2";
 import { RxCross2 } from "react-icons/rx";
+import { motion } from "framer-motion";
 const Header = () => {
-  const [showNavbar, setShowNavbar] = useState(false);
-
+  const [isNavBarShowing, setIsNavBarShowing] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
   const toggleNavbar = () => {
-    setShowNavbar(!showNavbar);
+    setIsNavBarShowing(!isNavBarShowing);
   };
-
+  const listItemVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+        delay: 0.2,
+      },
+    },
+  };
+  const listContactVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+        delay: 0.4,
+      },
+    },
+  };
+  const listLinkVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+        delay: 0.6,
+      },
+    },
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  useEffect(() => {}, [isMobile]);
+  useEffect(() => {}, [isNavBarShowing]);
   return (
-    <header className="w-full fixed flex items-center justify-between px-7 py-7 md:p-20 bg-transparent">
-      {!showNavbar ? (
-        <Link href="/" className="text-5xl text-white font-semibold z-[1000]">
-          Logo
-        </Link>
+    <header className="w-full fixed flex items-center justify-between px-7 py-7 md:p-20 bg-transparent z-[100]">
+      {!isNavBarShowing ? (
+        <motion.div className="text-5xl text-white font-semibold z-[1000]">
+          <Link href="/">Logo</Link>
+        </motion.div>
       ) : (
-        <Link
-          href="/"
-          className="text-5xl text-black md:text-white font-semibold z-[1000]"
-        >
-          Logo
-        </Link>
+        <motion.div className="text-5xl text-black md:text-white font-semibold z-[1000]">
+          <Link href="/">Logo</Link>
+        </motion.div>
       )}
 
-      {!showNavbar ? (
+      {!isNavBarShowing ? (
         <button className="text-slate-300 z-[999]" onClick={toggleNavbar}>
           <HiMiniBars3BottomRight size={40} />
         </button>
@@ -38,51 +80,74 @@ const Header = () => {
         </button>
       )}
 
-      {showNavbar && (
-        <nav className="text-xl md:text-base w-full h-full top-0 right-0 pt-28 px-14 md:w-auto md:h-auto md:pt-14 md:pb-7 md:px-7 bg-white text-black fixed md:right-[60px] md:top-[80px] z-[998]">
-          <ul className="mt-6 mx-2">
-            <li className="mb-6 md:my-2">
-              <a href="/work">My Works</a>
+      <motion.nav
+        initial={
+          !isMobile
+            ? { opacity: 0, scale: 0.3, originX: 1, originY: 0 }
+            : { opacity: 0 }
+        }
+        animate={isNavBarShowing ? { opacity: 1, scale: 1 } : { opacity: 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        style={{ pointerEvents: isNavBarShowing ? "auto" : "none" }}
+        className="border rounded-sm shadow-md shadow-slate-800 text-xl md:text-base w-full h-full top-0 right-0 pt-28 px-14 md:w-auto md:h-auto md:p-[3.5em] bg-white text-black fixed md:right-[60px] md:top-[80px] z-[998]"
+      >
+        <motion.ul
+          variants={listItemVariants}
+          initial={isNavBarShowing ? "hidden" : "visible"}
+          animate={isNavBarShowing ? "visible" : "hidden"}
+          className="my-[1.2em] leading-[2.5] text-[1.15em]"
+        >
+          <li className="mb-6 md:my-2">
+            <a href="/work">My Self</a>
+          </li>
+          <li className="mb-6 md:my-2">
+            <a href="/work">My Works</a>
+          </li>
+          <li className="mb-6 md:my-2">
+            <a href="/resume">My Résumé</a>
+          </li>
+        </motion.ul>
+        <motion.div
+          variants={listContactVariants}
+          initial={isNavBarShowing ? "hidden" : "visible"}
+          animate={isNavBarShowing ? "visible" : "hidden"}
+          className="mt-[0.8em] leading-[2.5] text-[1em] pb-[2em]"
+        >
+          <span className="text-slate-400 tracking-widest">SAY HELLO</span>
+          <ul>
+            <li className="my-3 md:my-2">
+              <a href="mailto:thedangqn@gmail.com">thedangqn@gmail.com</a>
             </li>
-            <li className="my-6 md:my-2">
-              <a href="/resume">My Resume</a>
+            <li className="my-3 md:my-2">
+              <a href="https://t.me/dynotran999" target="_blank">
+                t.me/dynotran999
+              </a>
             </li>
           </ul>
-          <div className="mt-32 md:mt-10 mx-2">
-            <span className="text-slate-400 tracking-widest">SAY HELLO</span>
-            <ul>
-              <li className="my-6 md:my-2">
-                <a href="mailto:thedangqn@gmail.com">thedangqn@gmail.com</a>
-              </li>
-              <li className="my-6 md:my-2">
-                <a href="https://t.me/dynotran999" target="_blank">
-                  t.me/dynotran999
-                </a>
-              </li>
-            </ul>
-          </div>
-          <ul className="mt-28 md:mt-10 mx-2">
-            <li className="inline-block mr-8">
-              <a href="https://www.facebook.com/dynotran999/" target="_blank">
-                <FaFacebookSquare size={30} />
-              </a>
-            </li>
-            <li className="inline-block mr-8">
-              <a href="https://github.com/tranthedang" target="_blank">
-                <FaGithubSquare size={30} />
-              </a>
-            </li>
-            <li className="inline-block mr-8">
-              <a
-                href="https://www.linkedin.com/in/dynotran999/"
-                target="_blank"
-              >
-                <FaLinkedin size={30} />
-              </a>
-            </li>
-          </ul>
-        </nav>
-      )}
+        </motion.div>
+        <motion.ul
+          variants={listLinkVariants}
+          initial={isNavBarShowing ? "hidden" : "visible"}
+          animate={isNavBarShowing ? "visible" : "hidden"}
+          className="flex"
+        >
+          <li className="mr-[2.5em]">
+            <a href="https://www.facebook.com/dynotran999/" target="_blank">
+              <FaFacebookSquare size={35} />
+            </a>
+          </li>
+          <li className="mr-[2.5em]">
+            <a href="https://github.com/tranthedang" target="_blank">
+              <FaGithubSquare size={35} />
+            </a>
+          </li>
+          <li className="mr-[2.5em]">
+            <a href="https://www.linkedin.com/in/dynotran999/" target="_blank">
+              <FaLinkedin size={35} />
+            </a>
+          </li>
+        </motion.ul>
+      </motion.nav>
     </header>
   );
 };
